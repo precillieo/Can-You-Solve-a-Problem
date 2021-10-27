243474252d55f493148cfee74b6b55618673892d
Shreekar Kolanu <43133388+Skillz619@users.noreply.github.com>
2021-10-27 10:55:50 +0530

    Update Factorial of Number.py

--- In-house/Factorial of Number.py
+++ In-house/Factorial of Number.py
@@ -1,16 +1,16 @@
-num = 7
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
+ 
 
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+num = 5;
+print("Factorial of",num,"is",
+factorial(num)

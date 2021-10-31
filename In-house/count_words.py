b26f59ca87638df24d801de622c90fe653e628fc
dimitrisz123 <dimitris-007@hotmail.com>
2021-10-31 22:14:55 +0200

    count word in a sentence

---
+++ In-house/count_words.py
@@ -0,0 +1,11 @@
+test_string = "The best Computer Science Portal"
+  
+# printing original string
+print ("The original string is : " +  test_string)
+  
+# using split()
+# to count words in string
+res = len(test_string.split())
+  
+# printing result
+print ("The number of words in string are : " +  str(res))
